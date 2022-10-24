c909eaf1dda6ee3af510c00839755fdf931b7c12
Andre <16894144+AndrevdG@users.noreply.github.com>
2022-10-24 20:13:50 +0000

    ...

--- default/.devcontainer/setuppwsh.ps1
+++ default/.devcontainer/setuppwsh.ps1
@@ -1,4 +1,5 @@
 # Silence the spammy behavior of Install-Module
 $ProgressPreference = "SilentlyContinue"
 # Install Az modules
+Write-Host "Installing Az module from PowerShell gallery"
 Install-Module Az -Force
